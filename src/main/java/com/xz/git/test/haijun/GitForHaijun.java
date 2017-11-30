f213a37fa1bcee4deea6ede6b07c76638f22cac3
wangqingwen <qingwen.wang@newtouch.cn>
2017-11-30 18:57:19 +0800

    11

--- src/main/java/com/xz/git/test/haijun/GitForHaijun.java
+++ src/main/java/com/xz/git/test/haijun/GitForHaijun.java
@@ -17,5 +17,7 @@ public class GitForHaijun {
 		System.out.println("test merge1 10");
 		System.out.println("test merge1 11");
 		System.out.println("test merge1 12");
+		System.out.println("test merge1 13");
+		System.out.println("test merge1 14");
 	}
 }
