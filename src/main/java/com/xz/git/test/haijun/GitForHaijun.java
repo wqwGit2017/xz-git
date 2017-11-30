06727f046a1ad6dc3bde0c450249783701a4ae5a
wangqingwen <qingwen.wang@newtouch.cn>
2017-11-30 16:20:29 +0800

    7

--- src/main/java/com/xz/git/test/haijun/GitForHaijun.java
+++ src/main/java/com/xz/git/test/haijun/GitForHaijun.java
@@ -3,5 +3,6 @@ package com.xz.git.test.haijun;
 public class GitForHaijun {
 	public static void main(String[] args) {
 		System.out.println("git test by haijun");
+		System.out.println("test merge");
 	}
 }
