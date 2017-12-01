e3ddd5388769ca3d5520ca73b7a8c0d87732d087
Administrator <Administrator@MICROSOFT>
2017-12-01 15:35:58 +0800

    dddwwww

--- src/main/java/com/xz/git/test/wqw/App.java
+++ src/main/java/com/xz/git/test/wqw/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World Haijun." );
+        System.out.println( "Hello World Haijun.111" );
         System.out.println( "Hello World WangQingwen." );
         
     }
