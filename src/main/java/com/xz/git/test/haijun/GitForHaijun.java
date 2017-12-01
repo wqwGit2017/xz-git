b5ab53e0344de9b62e87c5829b29b533857d6ff8
Administrator <Administrator@MICROSOFT>
2017-12-01 14:45:19 +0800

    测试01

--- src/main/java/com/xz/git/test/haijun/GitForHaijun.java
+++ src/main/java/com/xz/git/test/haijun/GitForHaijun.java
@@ -23,5 +23,7 @@ public class GitForHaijun {
 		System.out.println("test merge1 13");
 		System.out.println("test merge1 14");
 		System.out.println("test merge1 14");
+		System.out.println("test merge1 15");
+		System.out.println("test merge1 16");
 	}
 }
