2a6579951ac478e40cbb1218e4c8edae69ba37c3
wangqingwen <qingwen.wang@newtouch.cn>
2017-12-01 10:24:44 +0800

    1-1

--- src/main/java/com/xz/git/test/haijun/GitForHaijun.java
+++ src/main/java/com/xz/git/test/haijun/GitForHaijun.java
@@ -3,22 +3,5 @@ package com.xz.git.test.haijun;
 public class GitForHaijun {
 	public static void main(String[] args) {
 		System.out.println("git test by haijun");
-		System.out.println("test merge");
-		System.out.println("test merge1");
-		System.out.println("test merge1 1");
-		System.out.println("test merge1 2");
-		System.out.println("test merge1 3");
-		System.out.println("test merge1 4");
-		System.out.println("test merge1 5");
-		System.out.println("test merge1 6");
-		System.out.println("test merge1 7");
-		System.out.println("test merge1 8");
-		System.out.println("test merge1 9");
-		System.out.println("test merge1 10");
-		System.out.println("test merge1 11");
-		System.out.println("test merge1 12");
-		System.out.println("test merge1 13");
-		System.out.println("test merge1 14");
-		System.out.println("test merge1 14");
 	}
 }
