083e0016e3b4d49b8ee7778ef55e1fb2ab4f735d
wangqingwen <qingwen.wang@newtouch.cn>
2017-12-04 11:14:13 +0800

    11

--- src/main/java/com/xz/git/test/kangmengmeng/GitForMengmeng.java
+++ src/main/java/com/xz/git/test/kangmengmeng/GitForMengmeng.java
@@ -3,7 +3,7 @@ package com.xz.git.test.kangmengmeng;
 public class GitForMengmeng {
 	
 	public static void main(String[] args) {
-		System.out.println("git test by mengmeng");
+		System.out.println("git test by mengmeng"); 
 	}
 
 }
